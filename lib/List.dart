851fb144895d8826e4031f69eb628f8670c05638
chenzhaojun <mrchenzhaojun@gmai.com>
2019-05-06 19:55:24 +0800

    feat(application): 增加ListView 和 GridView 的监听功能，熟悉 NotificationListener 以及 ScrollController 的使用

--- lib/List.dart
+++ lib/List.dart
@@ -4,81 +4,103 @@ import 'package:english_words/english_words.dart';
 class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: new Text("ListView"),  
-      ),
-      body: new MyListView(),
-    );
+    return new MyListView();
   }
 }
 
-class MyListView  extends StatelessWidget{
+class MyListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(title: Text("this is ListView Title")),
-        Expanded(
-          child: InfiniteListView(),
-        )
-      ],
-    );
+    return InfiniteListView();
   }
 }
 
-class InfiniteListView extends StatefulWidget{
-
+class InfiniteListView extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _InfiniteListViewState();
-  
-  }
-  
-class _InfiniteListViewState  extends State<InfiniteListView>{
-    
-    static const loadingTag = "##loadingTag##";
-    var _words =<String>[loadingTag];
+}
+
+class _InfiniteListViewState extends State<InfiniteListView> {
+  ScrollController _controller = new ScrollController();
+  static const loadingTag = "##loadingTag##";
+  var _words = <String>[loadingTag];
+  bool showFloatButton = false;
 
   @override
   void initState() {
     _retrieveData();
+    _controller.addListener(() {
+      print(_controller.offset);
+      if (_controller.offset < 500 && showFloatButton) {
+        setState(() {
+          showFloatButton = false;
+        });
+      } else if (_controller.offset >= 500 && showFloatButton == false) {
+        setState(() {
+          showFloatButton = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return ListView.separated(
-      itemCount: _words.length,
-      itemBuilder: (context,index){
-        if (_words[index]==loadingTag) {
-
-          if (_words.length-1<100) {
-             _retrieveData();
-                return Container(
-                  padding: const EdgeInsets.all(16.0),
-                  alignment: Alignment.center,
-                  child: SizedBox(width: 24.0,height: 24.0,child: CircularProgressIndicator(strokeWidth: 2.0)),
-                );
-          } else {
+    return Scaffold(
+      appBar: AppBar(title: Text("ListView")),
+      body: Scrollbar(
+          child: ListView.separated(
+        controller: _controller,
+        itemCount: _words.length,
+        itemBuilder: (context, index) {
+          if (_words[index] == loadingTag) {
+            if (_words.length - 1 < 100) {
+              _retrieveData();
+              return Container(
+                padding: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: SizedBox(
+                    width: 24.0,
+                    height: 24.0,
+                    child: CircularProgressIndicator(strokeWidth: 2.0)),
+              );
+            } else {
               return Container(
                 alignment: Alignment.center,
                 padding: const EdgeInsets.all(16.0),
-                child: Text("没有更多了",style:TextStyle(color: Colors.blue)),
-                );
-               }
-              }
-            return ListTile(title: Text(_words[index]),);
-             },
-       separatorBuilder: (context, index) => Divider(height: .0),
-            );  
+                child: Text("没有更多了", style: TextStyle(color: Colors.blue)),
+              );
+            }
           }
-        
-          void _retrieveData() {
-                  Future.delayed(Duration(seconds: 2)).then((data){
-                    _words.insertAll(_words.length-1, generateWordPairs().take(20).map((e)=>e.asPascalCase).toList());
-                  setState(() {
-                    
-                  });
-                  });
-          }
+          return ListTile(
+            title: Text(_words[index]),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(height: .0),
+      )),
+      floatingActionButton: !showFloatButton
+          ? null
+          : FloatingActionButton(
+              child: Icon(Icons.arrow_upward),
+              onPressed: () {
+                //返回到顶部时执行动画
+                _controller.animateTo(.0,
+                    duration: Duration(milliseconds: 200), curve: Curves.ease);
+              },
+            ),
+    );
+  }
+
+  void _retrieveData() {
+    Future.delayed(Duration(seconds: 2)).then((data) {
+      _words.insertAll(_words.length - 1,
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList());
+      setState(() {});
+    });
+  }
 }
--- lib/gridViewView.dart
+++ lib/gridViewView.dart
@@ -8,36 +8,55 @@ class GridViewPreview extends StatelessWidget {
     );
   }
 }
+
 class InfiniteGridView extends StatefulWidget {
   @override
   _InfiniteGridViewState createState() => new _InfiniteGridViewState();
 }
 
 class _InfiniteGridViewState extends State<InfiniteGridView> {
-
   List<IconData> _icons = []; //保存Icon数据
-
+  String _progress = "0%";
   @override
   void initState() {
-    // 初始化数据  
+    // 初始化数据
     _retrieveIcons();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, //每行三列
-            childAspectRatio: 1.0 //显示区域宽高相等
-        ),
-        itemCount: _icons.length,
-        itemBuilder: (context, index) {
-          //如果显示到最后一个并且Icon总数小于200时继续获取数据
-          if (index == _icons.length - 1 && _icons.length < 200) {
-            _retrieveIcons();
-          }
-          return Icon(_icons[index]);
-        }
+    return Scrollbar(
+      child: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification notification) {
+            double progress = notification.metrics.pixels /
+                notification.metrics.maxScrollExtent;
+            setState(() {
+              _progress = "${(progress * 100).toInt()}%";
+            });
+          },
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3, //每行三列
+                      childAspectRatio: 1.0 //显示区域宽高相等
+                      ),
+                  itemCount: _icons.length,
+                  itemBuilder: (context, index) {
+                    //如果显示到最后一个并且Icon总数小于200时继续获取数据
+                    if (index == _icons.length - 1 && _icons.length < 200) {
+                      _retrieveIcons();
+                    }
+                    return Icon(_icons[index]);
+                  }),
+              CircleAvatar(
+                radius: 30.0,
+                child: Text(_progress),
+                backgroundColor: Colors.black54,
+              )
+            ],
+          )),
     );
   }
 
@@ -49,7 +68,8 @@ class _InfiniteGridViewState extends State<InfiniteGridView> {
           Icons.ac_unit,
           Icons.airport_shuttle,
           Icons.all_inclusive,
-          Icons.beach_access, Icons.cake,
+          Icons.beach_access,
+          Icons.cake,
           Icons.free_breakfast
         ]);
       });
